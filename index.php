4a0ec7ecc61bb51e85fa06b82f52426657c2cc11
AlexandreBasei <111810502+AlexandreBasei@users.noreply.github.com>
2023-02-02 20:40:43 +0100

    Upload
    Premier upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/pages/index.css">
+    <title>Alexandre Basei - Portfolio</title>
+</head>
+
+<body>
+    <header>
+        <button id="burger" onclick="addClass('burger','caché');addClass('navigation','active');rmClass('burger2','caché')">
+            <div></div>
+            <div></div>
+            <div></div>
+        </button>
+        <button id="burger2" class="caché" onclick="addClass('burger2','caché');rmClass('burger','caché');rmClass('navigation','active')">⛌</button>
+        <nav id="navigation">
+            <a href="index.php" target="_self" rel="noopener noreferrer">Accueil</a>
+            <a href="real.htm" target="_self" rel="noopener noreferrer">Mes réalisations</a>
+        </nav>
+    </header>
+
+    <main>
+
+        <section id="title-container">
+        <h1 id="portfolio">Portfolio</h1>
+            <h1>Alexandre<br>Basei</h1>
+            
+        </section>
+
+        <h2>Bienvenue !</h2><br>
+        <p>
+            Bonjour ! Je m’appelle Alexandre Basei, j’ai 19 ans et je vis à Paris. Attiré depuis toujours par la création de sites web et par l’audiovisuel, j’ai décidé de suivre une formation sur les Métiers du Multimédia et de l’Internet (MMI) à l’IUT de Vélizy. J’étudie la création de sites web, de leur conception à leur développement, le graphisme, la communication, l’audiovisuel et la création d’environnements interactifs. <br><br>
+
+            J’ai aussi décidé de réaliser ce BUT car la formation est pluridisciplinaire, ce qui me permet d’étudier de nombreux domaines et d’être polyvalent. J’ai déjà beaucoup appris depuis le début de la formation, que ce soit au niveau de la maîtrise logicielle ( Photoshop, InDesign, After Effects, Unity…) ou de la maîtrise de langages web comme le HTML, le CSS, le PHP et le JavaScript. Je sais aussi utiliser certains frameworks comme Angular.js, Vue.js ou Symfony. Enfin, j’ai une bonne maîtrise des langages de programmation que le Java ou le C#. <br><br>
+
+            J’ai déjà réalisé plusieurs projets, comme la conception d’une marque, de nombreux sites web ou la réalisation de deux documentaires. Sur la page Mes Réalisations, vous pourrez avoir un aperçu plus détaillé de mes projets.
+        </p>
+        <h3>Bonne visite !</span>
+
+            <h2>ME CONTACTER</h2>
+
+            <section>
+                <h3>Mail</h3>
+                <span>alexandreacevedobasei@gmail.com</span>
+
+                <h3>Téléphone</h3>
+                <span>06 46 88 46 88</span>
+
+                <h3>Linkedin</h3>
+                <a href="https://www.linkedin.com/in/alexandre-basei" target="_blank" rel="noopener noreferrer">linkedin.com/in/alexandre-basei</a>
+
+                <form method="post">
+                    <label for="source">Votre mail :</label>
+                    <input type="text" name="source">
+                    <label for="obj">Objet</label>
+                    <input type="text" name="obj">
+                    <label for="msg">Message</label>
+                    <textarea name="msg" cols="30" rows="10"></textarea>
+                    <input type="submit" id="envoi" value="Envoyer">
+                </form>
+            </section>
+
+
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+    <script src="./js/src/functions.js"></script>
+    <script src="./js/src/main.js"></script>
+</body>
+
+</html>
---
+++ js/src/functions.js
@@ -0,0 +1,7 @@
+function addClass(el,classe) {
+    document.getElementById(el).classList.add(classe);
+}
+
+function rmClass(el,classe) {
+    document.getElementById(el).classList.remove(classe);
+}
