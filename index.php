656cbc1cd9aa62b1ff98dda6fdb295faa5ef7172
Alexandre Basei <alexandreacevedobasei@gmail.com>
2023-02-18 22:02:32 +0100

    +phpmailer

--- index.php
+++ index.php
@@ -1,17 +1,50 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 if (isset($_POST["msg"])) {
 
-    $to = "alexandreacevedobasei@gmail.com";
-    $subject = $_POST["obj"];
-    $message = $_POST["msg"];
-    $headers = "From: " . $_POST["source"] . "r\n";
+    $mail = new PHPMailer(true);
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully.";
-    } else {
-        echo "Failed to send email.";
+    try {
+        //Server settings
+        $mail->SMTPDebug = false;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'servd162214.srv.odns.fr';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'abasei@alexandre.basei.mmi-velizy.fr';                     //SMTP username
+        $mail->Password   = 'v45LV*yQ#uWpvZr';                               //SMTP password
+        $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom($_POST["source"], $_POST["source"]);
+        $mail->addAddress('abasei@alexandre.basei.mmi-velizy.fr');     //Add a recipient
+        // $mail->addReplyTo('info@example.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        $mail->addBCC('alexandreacevedobasei@gmail.com');
+
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $_POST["obj"];
+        $mail->Body    = $_POST["msg"];
+        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo "<script>console.log('Email sent successfully.');</script>";
+    } catch (Exception $e) {
+        echo "<script>console.log('Email sent successfully.  Mailer Error: {$mail->ErrorInfo}');</script>";
     }
+
 }
 
 ?>
