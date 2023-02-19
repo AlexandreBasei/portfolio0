682b330e294ea467ccb9f560d0c413d599ec9969
Alexandre Basei <alexandreacevedobasei@gmail.com>
2023-02-19 12:11:05 +0100

    +favicon

--- index.php
+++ index.php
@@ -58,8 +58,8 @@ if (isset($_POST["msg"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/uicons-thin-straight/css/uicons-thin-straight.css">
     <link rel="stylesheet" href="./css/pages/index.css">
+    <link rel="icon" type="image/png" href="favicon.png" />
     <title>Alexandre Basei - Portfolio</title>
 </head>
 
